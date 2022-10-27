314d464cffb1ed13028966f62c399f88e86d1a66
unknown <70929180+jasonliny@users.noreply.github.com>
2022-10-27 04:44:25 -0400

    Reset/Setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from Home.js
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: `/book/:id`,
+    element: <BookListing />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={router} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/BookCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function BookCard() {
+  return <div></div>;
+}
+export default BookCard;
